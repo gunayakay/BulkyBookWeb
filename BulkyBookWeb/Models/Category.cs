360522df810b2073a524cbd505af5d9fb18c94dc
gunayakay <gunayakay@outlook.com>
2023-02-17 22:26:58 +0300

    Add Data Annotations

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,11 +1,20 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace BulkyBookWeb.Models
 {
 	public class Category
 	{
-	public int Id { get; set; }
-	public String Name{ get; set; }
-	public int DisplayOrder { get; set; }
+		[Key]
+
+		public int Id { get; set; }
+
+		[Required]
+
+		public String Name{ get; set; }
+
+		public int DisplayOrder { get; set; }
+
 		public DateTime CreatedDateTime { get; set; } = DateTime.Now;
 	}
 }
